272247527a99bc6414743099ab34cf0886a2cc8c
Teu  Phomthichack <c8055808@>
2021-09-29 14:43:42 -0500

    Test the pseudo-code for number 1

---
+++ pseudo-test1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+int main()
+{
+	int x = 100;
+	int y = 200;
+	int z = 500;
+	int largest;
+	int smallest;
+
+	if ((x > y ) && ( x > z))
+	{
+		largest = x;
+	}
+	else if ((y > x) && ( y > z))
+	{
+		largest = y;
+	}
+	else
+		largest = z;
+
+	if ((y = largest) || (z = largest))
+	{
+		smallest = x;
+	}
+	else if ((x = largest) || (z = largest))
+	{
+		smallest = y;
+	}
+	else
+		smallest = z;
+
+
+	std::cout << "Largest: " << largest << std::endl;
+	std::cout << "Smallest: " << smallest << std::endl;
+}
